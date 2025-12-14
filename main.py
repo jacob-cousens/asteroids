8c1ff45b2fa7e2f9e7c76b5168e798240da86b91
jacob-cousens <jacob.a.cousens@gmail.com>
2025-12-14 12:09:12 +1100

    Add base class CircleShape for game objects, including Player

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+import os
+os.environ["SDL_AUDIODRIVER"] = "dummy"
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 from logger import log_state
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -15,7 +22,11 @@ def main():
                 return
 
         screen.fill((0, 0, 0))
+
+        player.draw(screen)
+        
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
